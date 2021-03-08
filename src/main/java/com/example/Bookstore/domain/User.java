6ff07f75477647105e2ad27829e22413042600f3
dkhlapov <khlapov99@gmail.com>
2021-03-08 12:53:23 +0200

    updated for launch on Heroku

--- src/main/java/com/example/Bookstore/domain/User.java
+++ src/main/java/com/example/Bookstore/domain/User.java
@@ -3,6 +3,7 @@ package com.example.Bookstore.domain;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "usertable")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
